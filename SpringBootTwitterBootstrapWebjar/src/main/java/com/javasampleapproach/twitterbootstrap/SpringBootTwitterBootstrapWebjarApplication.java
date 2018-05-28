50ae86420304be13a8dd62820f9c6c674db45b9d
ashu.jain <ashu.jain01@gmail.com>
2018-05-28 14:43:54 +0530

    joioi

---
+++ SpringBootTwitterBootstrapWebjar/src/main/java/com/javasampleapproach/twitterbootstrap/SpringBootTwitterBootstrapWebjarApplication.java
@@ -0,0 +1,12 @@
+package com.javasampleapproach.twitterbootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTwitterBootstrapWebjarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTwitterBootstrapWebjarApplication.class, args);
+	}
+}
